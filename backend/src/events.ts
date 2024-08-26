b9ba4b4f3af6cf506500094509cea7a7975249e0
emirhangunec <gunecdevelopment@gmail.com>
2024-08-27 01:12:35 +0300

    fix events middleware

--- backend/src/events.ts
+++ backend/src/events.ts
@@ -1,31 +1,18 @@
 import {EventEmitter} from "node:events";
 
-type EventMap<T> = Record<keyof T, any[]> | DefaultEventMap;
-type DefaultEventMap = [never];
 
-export class WildcardEventEmitter<T extends EventMap<T> = DefaultEventMap> extends EventEmitter<T> {
-
-    emit<K>(eventName: T extends [never] ? string | symbol : keyof T | K, ...args: T extends [never] ? any[] : K extends keyof T ? T[K] : never): boolean {
-        //@ts-expect-error
-        super.emit('*', eventName, ...args);
-        return super.emit(eventName, ...args);// || super.emit('', ...args);
+class EventHandler extends EventEmitter {
+    constructor() {
+        super();
     }
 
-    //@ts-expect-error
-    on(eventName: '*', listener: (eventName: keyof T, ...args: any[]) => void): this;
-    //@ts-expect-error
-    on<K>(eventName: T extends [never] ? string | symbol : (keyof T) | K, listener: T extends [never] ? (...args: any[]) => void : K extends keyof T ? T[K] extends unknown[] ? (...args: T[K]) => void : never : never): this {
-        if(eventName === '*')
-            //@ts-expect-error
-            return super.on('*', ()=>listener(eventName, ...args))
-        return super.on(eventName, listener);
+    emit(eventName: string | symbol, ...args: any) {
+        console.log(`Event emitted: ${eventName.toString} with args: ${args}`);
+        return super.emit(eventName, ...args);
     }
 
 }
-const eventHandler = new WildcardEventEmitter();
 
-eventHandler.on('*', (eventName, ...args) => {
-    console.log(`Event ${eventName.toString()} was emitted with args: ${args}`);
-})
+const eventHandler = new EventHandler();
 
 export default eventHandler;
